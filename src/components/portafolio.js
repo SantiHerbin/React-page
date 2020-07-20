8618fe3ac77832cf72ab9ef523a000c39a098709
Santiago <santi_herbin@hotmail.com>
2020-07-20 00:28:58 -0300

    En proceso pagina de portafolio

--- src/components/portafolio.js
+++ src/components/portafolio.js
@@ -1,11 +1,30 @@
-import React, {ReactDOM} from 'react'
-import { render } from '@testing-library/react'
+import React, {ReactDOM} from 'react';
+import { render } from '@testing-library/react';
 
-import portafolio from './styles/portafolio.css'
+import portafolio from './styles/portafolio.css';
+import tsp from '../images/tsprogramacion.PNG';
 
 function Portafolio() {
     return(
-        <h1>Portafolio Page</h1>
+            <div className="contain">
+                <div className="content">
+                    <div className="column">
+                        <div className="col-sm">
+                            <img src={tsp} width="50px" height="40px" alt=""/>
+                            <h3>TSProgramacion</h3>
+                            <p>Todo bien</p>
+                        </div>
+                        <div className="col-sm">
+                            <img src="" alt=""/>
+                            <p>Hola que tal!!</p>
+                        </div>
+                        <div className="col-sm">
+                            <img src="" alt=""/>
+                            <p>Hola que tal!!</p>
+                        </div>
+                </div>
+                </div>
+            </div>
     );
 }
 
